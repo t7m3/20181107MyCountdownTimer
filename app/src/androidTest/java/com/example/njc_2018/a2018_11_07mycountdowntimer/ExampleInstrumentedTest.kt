324568993c09c10cbde0abe24ff54b119e174ec4
NJC-T1 <toda7.mk3@gmail.com>
2018-11-08 11:23:01 +0900

    最初のコミット（画面を作成する）2018-11-08

---
+++ app/src/androidTest/java/com/example/njc_2018/a2018_11_07mycountdowntimer/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.njc_2018.a2018_11_07mycountdowntimer
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.njc_2018.a2018_11_07mycountdowntimer", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/example/njc_2018/a2018_11_07mycountdowntimer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.njc_2018.a2018_11_07mycountdowntimer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
