fa5578132f72dc97847187293d8303c189a5cfb5
user224 <toda7.mk3@gmail.com>
2018-11-29 21:52:34 +0900

    2018-11-29 ２回目のコミット ---ここからアニメーション

--- app/src/main/java/com/example/njc_2018/a2018_11_07mycountdowntimer/MainActivity.kt
+++ app/src/main/java/com/example/njc_2018/a2018_11_07mycountdowntimer/MainActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.util.DisplayMetrics
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var soundPool: SoundPool
     private  var soundResID = 0
+    private var dir = 1  //アニメーション用の変数の宣言
 
     inner class MyCountDownTimer(millisInFuture: Long, countDownInterval: Long) :
         CountDownTimer(millisInFuture, countDownInterval) {
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
             val minute = millisUntilFinished / 1000L / 60L
             val second = millisUntilFinished / 1000 % 60
             timerText.text = "%1d:%2$02d".format(minute, second)
+
+            enemyMove(5)
+
         }
 
         override fun onFinish() {
@@ -39,6 +46,10 @@ class MainActivity : AppCompatActivity() {
         val timer = MyCountDownTimer(3 * 60 * 1000, 100)
         //timerText.text = "0:20"                                                             //20秒　デバッグ用
         //val timer = MyCountDownTimer(2 * 10 * 1000, 100)     //20秒　デバッグ用
+
+        // imageViewEnemy の初期位置の設定
+        imageViewEnemy.x = 50F
+        imageViewEnemy.y = 100F
 
         playStop.setOnClickListener {
             when (timer.isRunning){
@@ -67,5 +78,22 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         soundPool.release()
+    }
+
+    //アニメーション用のメソッド
+    fun enemyMove(x: Int){
+
+        val dMetrics = DisplayMetrics()  //DisplayMetrics のインスタンスを生成する
+        windowManager.defaultDisplay.getMetrics(dMetrics)  //スクリーンサイズを取得しているらしい
+
+        val screenWidth = dMetrics.widthPixels  //スクリーンの幅を取得
+        val screenHeight = dMetrics.heightPixels  //スクリーンの高さを取得
+
+        imageViewEnemy.x = imageViewEnemy.x + x * dir
+
+        if(imageViewEnemy.x < 0 ||  screenWidth - imageViewEnemy.width < imageViewEnemy.x ){
+            dir = dir * -1; //移動の左右の向きを反転する
+        }
+
     }
 }
